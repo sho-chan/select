71aa88bd13862f889a1eb6895e54026782238adc
石川大輔 <daisuke@ishikawadaisuke-no-MacBook-Pro.local>
2016-07-30 00:43:23 +0900

    full complete

--- Gemfile
+++ Gemfile
@@ -26,6 +26,7 @@ gem 'sdoc', '~> 0.4.0', group: :doc
 gem 'better_errors'
 
 gem 'twitter'
+gem 'instagram'
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
 
--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,11 +1,4 @@
 class HomesController < ApplicationController
-	require'twitter'
 	def index
-		@client = Twitter::REST::Client.new do |config|
-    	config.consumer_key        = '1NjdQJfDQEpKcxz9SQ9PXXzo7'
-    	config.consumer_secret     = 'uuzYItNYFqG767qy455VzhIlD4wMcnzLwAL16PlQq2F2VIvcTy'
-    	config.access_token        = '2400827238-xogkXd5UtO2NjgWDUxmoCn38fMsBC4a1TDHIEnG'
-    	config.access_token_secret = 'bE6EkWbuHXwXeu9GJQyhedlrNPc2Wgg0UA6lTVXfYRG3S'
-		end
 	end
 end
